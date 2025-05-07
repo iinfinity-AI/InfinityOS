5a8f5fd6ec7d9443ad65d5e094005db6953a0858
dinith-rusiru <it22145284@my.sliit.lk>
2025-05-07 15:59:21 +0530

    .

---
+++ server/index.js
@@ -0,0 +1,15 @@
+require('dotenv').config();
+const express = require('express');
+const mongoose = require('mongoose');
+
+const app = express();
+app.use(express.json());
+
+mongoose.connect(process.env.MONGODB_URL)
+  .then(() => console.log(' MongoDB Connected'))
+  .catch(err => console.error(' MongoDB connection error:', err));
+
+app.get('/', (req, res) => res.send('API is running...'));
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(` Server running on port ${PORT}`));
