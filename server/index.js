4ff46c50287fd2538ccf3d6b21a1b86fcc454569
Kasun_Udayanga <udayangakasun696@gmail.com>
2025-05-08 10:53:53 +0530

    add db configuration and update package.json to use nodemon

--- server/index.js
+++ server/index.js
@@ -1,15 +1,22 @@
-require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
+
+
+
+require('dotenv').config();
+
+
 
 const app = express();
 app.use(express.json());
 
+
+
 mongoose.connect(process.env.MONGODB_URL)
-  .then(() => console.log(' MongoDB Connected'))
+  .then(() => console.log('MongoDB Connected'))
   .catch(err => console.error(' MongoDB connection error:', err));
 
 app.get('/', (req, res) => res.send('API is running...'));
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(` Server running on port ${PORT}`));
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
