fd6fb14906717e880e2d019dfaced20d0454056f
@sanduniwathsala0123 <sanduniwathsala0123@gmail.com>
2025-05-08 18:22:00 +0530

    Created the react+vite+tailwind css project

---
+++ employeemgtsystem/src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="bg-blue-500 text-white p-6 text-center rounded-lg">
+      <h1 className="text-2xl font-bold">Hello Tailwind!</h1>
+    </div>
+  );
+}
+
+export default App;
