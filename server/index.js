ed6acb296367f5df8ab75d6ab7082f5c4c5524f0
Kasun_Udayanga <udayangakasun696@gmail.com>
2025-05-08 11:10:05 +0530

    implement MongoDB connection logic in separate module

--- server/index.js
+++ server/index.js
@@ -1,22 +1,18 @@
 const express = require('express');
-const mongoose = require('mongoose');
+const connectDB = require('./config/db');
+
 
 
 
 require('dotenv').config();
 
 
-
 const app = express();
 app.use(express.json());
+connectDB();
 
-
-
-mongoose.connect(process.env.MONGODB_URL)
-  .then(() => console.log('MongoDB Connected'))
-  .catch(err => console.error(' MongoDB connection error:', err));
 
 app.get('/', (req, res) => res.send('API is running...'));
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(PORT, () => console.log(`Server running on port http://localhost:${PORT}`));
