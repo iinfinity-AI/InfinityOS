b9331f798e5f4f4aaf6cd77dbdb9ae09e709f8e2
dinith-rusiru <it22145284@my.sliit.lk>
2025-05-09 15:00:57 +0530

    Create Mood  model

--- server/index.js
+++ server/index.js
@@ -6,7 +6,7 @@ const cookieParser = require('cookie-parser');
 const routes = require('./routes/auth');
 const taskRoutes = require("./routes/taskRoutes");
 dotenv.config();
-
+const moodRoutes = require("./routes/moodRoutes");
 
 const app = express();
 
@@ -22,6 +22,7 @@ connectDB();
 
 app.use("/api", routes);
 app.use("/api", taskRoutes);
+app.use("/api", moodRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
