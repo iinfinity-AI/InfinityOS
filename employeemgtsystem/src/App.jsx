f7c498ce6a66e068deb87df1b71a7b16970f800f
S19387 <dasuni2018@gmail.com>
2025-05-09 22:26:14 -0700

    update conflict

--- employeemgtsystem/src/App.jsx
+++ employeemgtsystem/src/App.jsx
@@ -1,12 +1,28 @@
 import TopNav from './components/navbar/TopNav';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import SignupPage from "./pages/signup/SignupPage";
+import LoginPage from "./pages/login/LoginPage";
+import ResetPassword from "./pages/login/ResetPassword";
+
 
 function App() {
   return (
     <div>
-      <TopNav />
-     
+      <TopNav /> 
+      {/* this navbar is not go with the login part just call it */}
+      <Router>
+        
+      <Routes>
+       
+        <Route path="/signup" element={<SignupPage />} />
+        <Route path="/login" element ={<LoginPage/>}/>
+        <Route path="/reset-password" element={<ResetPassword/>}/>
+      </Routes>
+    </Router>
+
+   </div>
       
-    </div>
+    
   );
 }
 
