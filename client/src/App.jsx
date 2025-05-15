7a315785715fee44a52c74597f1169e5b4a27dd1
@sanduniwathsala0123 <sanduniwathsala0123@gmail.com>
2025-05-15 08:02:20 +0530

    Updated homepage again

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,22 +5,18 @@ import LoginPage from "./pages/login/LoginPage";
 import ResetPassword from "./pages/login/ResetPassword";
 import HomePage from './pages/homepage/HomePage';
 
-
 function App() {
   return (
-    <div>
+   
       <Router>
-      <Routes>
-        <Route path="/" element={<HomePage/>} />
-        <Route path="/signup" element={<SignupPage />} />
-        <Route path="/login" element ={<LoginPage/>}/>
-        <Route path="/reset-password" element={<ResetPassword/>}/>
-      </Routes>
-    </Router>
-
-   </div>
-      
-    
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/signup" element={<SignupPage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/reset-password" element={<ResetPassword />} />
+        </Routes>
+      </Router>
+   
   );
 }
 
