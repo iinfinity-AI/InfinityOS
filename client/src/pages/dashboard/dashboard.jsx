d244d00e733ccf3f5373445d770ea65b489b0307
Kasun_Udayanga <udayangakasun696@gmail.com>
2025-05-17 20:50:30 +0530

    moods add

--- client/src/pages/dashboard/dashboard.jsx
+++ client/src/pages/dashboard/dashboard.jsx
@@ -140,7 +140,7 @@ const TaskDashboard = () => {
           </button>
           <button
             className="bg-green-600 text-white px-4 py-2 rounded shadow hover:bg-green-700 transition"
-            onClick={() => navigate("/mood")}
+            onClick={() => navigate("/moods")}
           >
             Mood
           </button>
